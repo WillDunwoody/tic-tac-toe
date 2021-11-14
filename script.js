4101a4f5fe31bb55fa23b6dff397a7d34cf1c66a
Will <will.dunwoody@gmail.com>
2021-11-14 22:02:41 +0100

    Completed foundations of programme

---
+++ script.js
@@ -0,0 +1,59 @@
+
+const gameBoard = (() => {
+    let gameArray = ['', '', '', '', '', '', '', '', '']
+    return { gameArray }
+})()
+
+const players = (name, choice) => {
+    return {name, choice}
+}
+
+let playerX = players("Will", 'x')
+let playerO = players("Tanja", 'o')
+
+const gameController = (() => {
+    let roundCount = 0
+    let player = ''
+
+    const playRound = () => {
+        
+        if(roundCount % 2 == 0) {
+            player = playerX.choice
+        } else {
+            player = playerO.choice
+        }
+        roundCount++
+    }
+
+    const playerChoice = () => {
+        return player 
+    }
+
+    return{ playRound, playerChoice }
+
+})()
+
+const displayController = (() => {
+    
+    const gridListener = (() => {
+        let gridBox = document.querySelectorAll('.gridBox')
+        
+        gridBox.forEach(element => {
+            element.addEventListener('click', function() {
+                console.log(gameController.playerChoice())
+                gameBoard.gameArray.splice(this.id, 1, gameController.playerChoice())
+                gameController.playRound()
+                updateBoard()
+            })
+        })
+    })()
+
+    const updateBoard = () => {
+        for(let val in gameBoard.gameArray) {
+            let gridBox = document.getElementById(val)
+            gridBox.innerText = gameBoard.gameArray[val]
+        }
+    }
+})()
+
+
